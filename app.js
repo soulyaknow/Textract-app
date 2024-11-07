cf821601249cccfe1af58a8d0abfb92729565024
Ranel Soliano <ranel.soliano@gmail.com>
2024-11-07 08:37:59 +0800

    latest commit with simplifyTextractResult

--- app.js
+++ app.js
@@ -26,17 +26,317 @@ const upload = multer({ storage });
 const bucketName = awsBucketName;
 
 // Function to get Textract job results
-const getTextractResults = async (jobId) => {
+const getFactFindTextractResults = async (jobId) => {
   return new Promise((resolve, reject) => {
     const interval = setInterval(async () => {
       try {
-        const response = await textract
+        // Check the job status
+        const jobResponse = await textract
           .getDocumentAnalysis({ JobId: jobId })
           .promise();
-        if (response.JobStatus === "SUCCEEDED") {
+
+        // If job succeeded, retrieve all pages
+        if (jobResponse.JobStatus === "SUCCEEDED") {
           clearInterval(interval);
-          resolve(response);
-        } else if (response.JobStatus === "FAILED") {
+
+          // Initialize array to store all blocks and pagination token
+          let allBlocks = [];
+          let nextToken = null;
+
+          do {
+            // Fetch results page by page using nextToken
+            const pageResponse = await textract
+              .getDocumentAnalysis({
+                JobId: jobId,
+                NextToken: nextToken,
+              })
+              .promise();
+
+            // Append blocks from each page to the result
+            allBlocks.push(...pageResponse.Blocks);
+
+            // Update nextToken for next page, or set to null if no more pages
+            nextToken = pageResponse.NextToken || null;
+          } while (nextToken);
+
+          resolve(allBlocks);
+        } else if (jobResponse.JobStatus === "FAILED") {
+          clearInterval(interval);
+          reject("Textract job failed.");
+        }
+      } catch (error) {
+        clearInterval(interval);
+        reject(error);
+      }
+    }, 5000); // Poll every 5 seconds
+  });
+};
+
+const getClientHandoverTextractResults = async (jobId) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(async () => {
+      try {
+        // Check the job status
+        const jobResponse = await textract
+          .getDocumentAnalysis({ JobId: jobId })
+          .promise();
+
+        // If job succeeded, retrieve all pages
+        if (jobResponse.JobStatus === "SUCCEEDED") {
+          clearInterval(interval);
+
+          // Initialize array to store all blocks and pagination token
+          let allBlocks = [];
+          let nextToken = null;
+
+          do {
+            // Fetch results page by page using nextToken
+            const pageResponse = await textract
+              .getDocumentAnalysis({
+                JobId: jobId,
+                NextToken: nextToken,
+              })
+              .promise();
+
+            // Append blocks from each page to the result
+            allBlocks.push(...pageResponse.Blocks);
+
+            // Update nextToken for next page, or set to null if no more pages
+            nextToken = pageResponse.NextToken || null;
+          } while (nextToken);
+
+          resolve(allBlocks);
+        } else if (jobResponse.JobStatus === "FAILED") {
+          clearInterval(interval);
+          reject("Textract job failed.");
+        }
+      } catch (error) {
+        clearInterval(interval);
+        reject(error);
+      }
+    }, 5000); // Poll every 5 seconds
+  });
+};
+
+const getDriverLicenseTextractResults = async (jobId) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(async () => {
+      try {
+        // Check the job status
+        const jobResponse = await textract
+          .getDocumentAnalysis({ JobId: jobId })
+          .promise();
+
+        // If job succeeded, retrieve all pages
+        if (jobResponse.JobStatus === "SUCCEEDED") {
+          clearInterval(interval);
+
+          // Initialize array to store all blocks and pagination token
+          let allBlocks = [];
+          let nextToken = null;
+
+          do {
+            // Fetch results page by page using nextToken
+            const pageResponse = await textract
+              .getDocumentAnalysis({
+                JobId: jobId,
+                NextToken: nextToken,
+              })
+              .promise();
+
+            // Append blocks from each page to the result
+            allBlocks.push(...pageResponse.Blocks);
+
+            // Update nextToken for next page, or set to null if no more pages
+            nextToken = pageResponse.NextToken || null;
+          } while (nextToken);
+
+          resolve(allBlocks);
+        } else if (jobResponse.JobStatus === "FAILED") {
+          clearInterval(interval);
+          reject("Textract job failed.");
+        }
+      } catch (error) {
+        clearInterval(interval);
+        reject(error);
+      }
+    }, 5000); // Poll every 5 seconds
+  });
+};
+
+const getPassportFileTextractResults = async (jobId) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(async () => {
+      try {
+        // Check the job status
+        const jobResponse = await textract
+          .getDocumentAnalysis({ JobId: jobId })
+          .promise();
+
+        // If job succeeded, retrieve all pages
+        if (jobResponse.JobStatus === "SUCCEEDED") {
+          clearInterval(interval);
+
+          // Initialize array to store all blocks and pagination token
+          let allBlocks = [];
+          let nextToken = null;
+
+          do {
+            // Fetch results page by page using nextToken
+            const pageResponse = await textract
+              .getDocumentAnalysis({
+                JobId: jobId,
+                NextToken: nextToken,
+              })
+              .promise();
+
+            // Append blocks from each page to the result
+            allBlocks.push(...pageResponse.Blocks);
+
+            // Update nextToken for next page, or set to null if no more pages
+            nextToken = pageResponse.NextToken || null;
+          } while (nextToken);
+
+          resolve(allBlocks);
+        } else if (jobResponse.JobStatus === "FAILED") {
+          clearInterval(interval);
+          reject("Textract job failed.");
+        }
+      } catch (error) {
+        clearInterval(interval);
+        reject(error);
+      }
+    }, 5000); // Poll every 5 seconds
+  });
+};
+
+const getNationalIDTextractResults = async (jobId) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(async () => {
+      try {
+        // Check the job status
+        const jobResponse = await textract
+          .getDocumentAnalysis({ JobId: jobId })
+          .promise();
+
+        // If job succeeded, retrieve all pages
+        if (jobResponse.JobStatus === "SUCCEEDED") {
+          clearInterval(interval);
+
+          // Initialize array to store all blocks and pagination token
+          let allBlocks = [];
+          let nextToken = null;
+
+          do {
+            // Fetch results page by page using nextToken
+            const pageResponse = await textract
+              .getDocumentAnalysis({
+                JobId: jobId,
+                NextToken: nextToken,
+              })
+              .promise();
+
+            // Append blocks from each page to the result
+            allBlocks.push(...pageResponse.Blocks);
+
+            // Update nextToken for next page, or set to null if no more pages
+            nextToken = pageResponse.NextToken || null;
+          } while (nextToken);
+
+          resolve(allBlocks);
+        } else if (jobResponse.JobStatus === "FAILED") {
+          clearInterval(interval);
+          reject("Textract job failed.");
+        }
+      } catch (error) {
+        clearInterval(interval);
+        reject(error);
+      }
+    }, 5000); // Poll every 5 seconds
+  });
+};
+
+const getBirthCertificateTextractResults = async (jobId) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(async () => {
+      try {
+        // Check the job status
+        const jobResponse = await textract
+          .getDocumentAnalysis({ JobId: jobId })
+          .promise();
+
+        // If job succeeded, retrieve all pages
+        if (jobResponse.JobStatus === "SUCCEEDED") {
+          clearInterval(interval);
+
+          // Initialize array to store all blocks and pagination token
+          let allBlocks = [];
+          let nextToken = null;
+
+          do {
+            // Fetch results page by page using nextToken
+            const pageResponse = await textract
+              .getDocumentAnalysis({
+                JobId: jobId,
+                NextToken: nextToken,
+              })
+              .promise();
+
+            // Append blocks from each page to the result
+            allBlocks.push(...pageResponse.Blocks);
+
+            // Update nextToken for next page, or set to null if no more pages
+            nextToken = pageResponse.NextToken || null;
+          } while (nextToken);
+
+          resolve(allBlocks);
+        } else if (jobResponse.JobStatus === "FAILED") {
+          clearInterval(interval);
+          reject("Textract job failed.");
+        }
+      } catch (error) {
+        clearInterval(interval);
+        reject(error);
+      }
+    }, 5000); // Poll every 5 seconds
+  });
+};
+
+const getMedicareTextractResults = async (jobId) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(async () => {
+      try {
+        // Check the job status
+        const jobResponse = await textract
+          .getDocumentAnalysis({ JobId: jobId })
+          .promise();
+
+        // If job succeeded, retrieve all pages
+        if (jobResponse.JobStatus === "SUCCEEDED") {
+          clearInterval(interval);
+
+          // Initialize array to store all blocks and pagination token
+          let allBlocks = [];
+          let nextToken = null;
+
+          do {
+            // Fetch results page by page using nextToken
+            const pageResponse = await textract
+              .getDocumentAnalysis({
+                JobId: jobId,
+                NextToken: nextToken,
+              })
+              .promise();
+
+            // Append blocks from each page to the result
+            allBlocks.push(...pageResponse.Blocks);
+
+            // Update nextToken for next page, or set to null if no more pages
+            nextToken = pageResponse.NextToken || null;
+          } while (nextToken);
+
+          resolve(allBlocks);
+        } else if (jobResponse.JobStatus === "FAILED") {
           clearInterval(interval);
           reject("Textract job failed.");
         }
@@ -51,7 +351,6 @@ const getTextractResults = async (jobId) => {
 // Function to send results to n8n webhook
 const sendToN8N = async (data) => {
   try {
-    console.log("Sending data:", data);
     const response = await axios.post(n8nWebhookUrl, data);
     console.log("Data sent to n8n successfully:", response.data);
   } catch (error) {
@@ -68,8 +367,45 @@ const sendToN8N = async (data) => {
   }
 };
 
+// Function to simplify the textract data
+const simplifyTextractResults = (textractData) => {
+  if (!textractData || !Array.isArray(textractData)) {
+    console.error("Textract data is undefined or not an array");
+    return [];
+  }
+
+  // Ensure textractData contains valid blocks with a BlockType
+  const blocks = textractData.filter((block) => block && block.BlockType);
+  if (!blocks.length) {
+    console.error("No blocks found in textract data");
+    return [];
+  }
+
+  // Process the blocks and return the simplified results
+  return blocks
+    .map((block) => {
+      if (block.BlockType === "LINE") {
+        return { text: block.Text || "", type: "LINE" };
+      } else if (block.BlockType === "KEY_VALUE_SET") {
+        const isKey = block.EntityTypes && block.EntityTypes.includes("KEY");
+        const isValue =
+          block.EntityTypes && block.EntityTypes.includes("VALUE");
+        const keyText = isKey ? block.Text || "Unknown Key" : undefined;
+        const valueText = isValue ? block.Text || "Unknown Value" : undefined;
+
+        return keyText && valueText
+          ? { text: `${keyText} : ${valueText}`, type: "KEY_VALUE_PAIR" }
+          : null;
+      }
+      return null;
+    })
+    .filter((entry) => entry !== null); // Remove any null entries
+};
+
 // POST endpoint to accept a file
 app.post("/upload", upload.single("file"), async (req, res) => {
+  const type = req.body.doc_type;
+
   if (!req.file) {
     return res.status(400).send("No file uploaded.");
   }
@@ -104,14 +440,126 @@ app.post("/upload", upload.single("file"), async (req, res) => {
     console.log("JobID:", jobId);
 
     // Poll for results
-    const textractResults = await getTextractResults(jobId);
+    switch (type) {
+      case "find_fact_file":
+        {
+          console.log("Extracting find_fact_file");
+          const textractResults = await getFactFindTextractResults(jobId);
+          const simplifiedResults = simplifyTextractResults(textractResults);
 
-    // Send the Textract results to n8n
-    await sendToN8N(textractResults);
+          // Send the Textract results to n8n
+          await sendToN8N(simplifiedResults);
 
-    res
-      .status(200)
-      .send({ message: "File processed successfully.", data: textractResults });
+          res.status(200).send({
+            message: "File processed successfully for find_fact_file.",
+            data: simplifiedResults,
+          });
+        }
+        break;
+
+      case "client_handover":
+        {
+          console.log("Extracting client_handover");
+          const textractResults = await getClientHandoverTextractResults(jobId);
+          const simplifiedResults = simplifyTextractResults(textractResults);
+
+          // Send the Textract results to n8n
+          await sendToN8N(simplifiedResults);
+
+          res.status(200).send({
+            message: "File processed successfully for client_handover.",
+            data: simplifiedResults,
+          });
+        }
+        break;
+
+      case "driver_license_file":
+        {
+          console.log("Extracting driver_license_file");
+          const textractResults = await getDriverLicenseTextractResults(jobId);
+          const simplifiedResults = simplifyTextractResults(textractResults);
+
+          // Send the Textract results to n8n
+          await sendToN8N(simplifiedResults);
+
+          res.status(200).send({
+            message: "File processed successfully for driver_license_file.",
+            data: simplifiedResults,
+          });
+        }
+        break;
+
+      case "passport_file":
+        {
+          console.log("Extracting passport_file");
+          const textractResults = await getPassportFileTextractResults(jobId);
+          const simplifiedResults = simplifyTextractResults(textractResults);
+          // Send the Textract results to n8n
+          await sendToN8N(simplifiedResults);
+
+          res.status(200).send({
+            message: "File processed successfully for passport_file.",
+            data: simplifiedResults,
+          });
+        }
+        break;
+
+      case "national_id_file":
+        {
+          console.log("Extracting national_id_file");
+          const textractResults = await getNationalIDTextractResults(jobId);
+          const simplifiedResults = simplifyTextractResults(textractResults);
+
+          // Send the Textract results to n8n
+          await sendToN8N(simplifiedResults);
+
+          res.status(200).send({
+            message: "File processed successfully for national_id_file.",
+            data: simplifiedResults,
+          });
+        }
+        break;
+
+      case "birth_cert_file":
+        {
+          console.log("Extracting birth_cert_file");
+          const textractResults = await getBirthCertificateTextractResults(
+            jobId
+          );
+          const simplifiedResults = simplifyTextractResults(textractResults);
+
+          // Send the Textract results to n8n
+          await sendToN8N(simplifiedResults);
+
+          res.status(200).send({
+            message: "File processed successfully for birth_cert_file.",
+            data: simplifiedResults,
+          });
+        }
+        break;
+
+      case "medicare_file":
+        {
+          console.log("Extracting medicare_file");
+          const textractResults = await getMedicareTextractResults(jobId);
+          const simplifiedResults = simplifyTextractResults(textractResults);
+
+          // Send the Textract results to n8n
+          await sendToN8N(simplifiedResults);
+
+          res.status(200).send({
+            message: "File processed successfully for medicare_file.",
+            data: simplifiedResults,
+          });
+        }
+        break;
+
+      default:
+        res.status(400).send({
+          message: "Invalid type specified.",
+        });
+        break;
+    }
   } catch (error) {
     console.error("Error processing file:", error);
     res.status(500).send("Error processing file.");
